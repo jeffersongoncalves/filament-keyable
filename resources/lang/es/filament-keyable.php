8b5d479cc3a5998e6a31badaf9f764172310a865
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-06-19 17:55:23 -0300

    feat(lang): add Spanish translations for API key resource

---
+++ resources/lang/es/filament-keyable.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Table Columns
+    |--------------------------------------------------------------------------
+    */
+
+    'column.key' => 'Clave API',
+    'column.name' => 'Nombre',
+    'column.keyable_id' => 'ID asignable',
+    'column.keyable_type' => 'Tipo asignable',
+    'column.last_used_at' => 'Último uso',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Infolist Fields
+    |--------------------------------------------------------------------------
+    */
+
+    'infolist.key' => 'Clave API',
+    'infolist.copy_message' => '¡Clave API copiada con éxito!',
+    'infolist.name' => 'Nombre',
+    'infolist.last_used_at' => 'Último uso',
+    'infolist.keyable_id' => 'ID asignable',
+    'infolist.keyable_type' => 'Tipo asignable',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Configuración',
+    'nav.operation.label' => 'Clave API',
+    'nav.operation.icon' => 'heroicon-o-key',
+    'resource.label.operation' => 'Clave API',
+    'resource.label.operations' => 'Claves API',
+];
